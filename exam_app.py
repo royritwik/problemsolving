0d47b6ab6d87ff49e7b553e354e08c8cdc39f74b
royritwik <136170158+royritwik@users.noreply.github.com>
2023-06-18 01:12:58 +0530

    Update exam_app.py

--- exam_app.py
+++ exam_app.py
@@ -1,58 +1,55 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import sqlite3
 
-import streamlit as st
-import snowflake.connector
+# Create a connection to the SQLite database
+conn = sqlite3.connect('survey.db')
 
-# Connect to Snowflake
-conn = snowflake.connector.connect(
-    user='<YOUR_USERNAME>',
-    password='<YOUR_PASSWORD>',
-    account='<YOUR_ACCOUNT_URL>',
-    warehouse='<YOUR_WAREHOUSE>',
-    database='<YOUR_DATABASE>',
-    schema='<YOUR_SCHEMA>'
-)
+# Create the survey table if it doesn't exist
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS survey (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question TEXT,
+        response TEXT
+    )
+'''
+conn.execute(create_table_query)
 
-# Load the questions from Snowflake
-def load_questions(num_questions):
-    query = f"SELECT * FROM questions LIMIT {num_questions}"
+# Load the questions from the database
+def load_questions():
+    query = "SELECT question FROM survey"
     cursor = conn.cursor()
     cursor.execute(query)
-    questions = cursor.fetchall()
+    questions = [row[0] for row in cursor.fetchall()]
     return questions
-
-# Validate the answers
-def validate_answers(answers):
-    query = f"SELECT * FROM questions"
-    cursor = conn.cursor()
-    cursor.execute(query)
-    questions = cursor.fetchall()
-    correct_count = sum(answers == row[5] for row in questions)
-    return correct_count
 
 # Main function
 def main():
     # Set up the app layout
-    st.title('MCQ Questions')
-    num_questions = st.slider('Select the number of questions to display', min_value=1, max_value=100, value=10)
-    questions = load_questions(num_questions)
+    st.title('Survey Form')
 
-    # Display the questions and collect user answers
-    answers = []
-    for idx, row in enumerate(questions):
-        st.subheader(f'Question {idx+1}: {row[1]}')
-        options = [row[2], row[3], row[4], row[5]]
-        selected_option = st.selectbox('Select your answer:', options)
-        answers.append(selected_option)
+    # Load the questions
+    questions = load_questions()
 
-    # Validate the answers
-    correct_count = validate_answers(answers)
+    # Display the form
+    with st.form(key='survey_form'):
+        for idx, question in enumerate(questions):
+            st.subheader(f'Question {idx+1}:')
+            st.write(question)
+            response = st.text_input(f'Response to Question {idx+1}')
+            st.write('---')
 
-    # Display the result
-    st.subheader('Result')
-    st.write(f'Correct answers: {correct_count} out of {num_questions}')
+        submitted = st.form_submit_button(label='Submit')
+
+        if submitted:
+            # Store the responses in the database
+            with conn:
+                for idx, question in enumerate(questions):
+                    response = st.session_state[f'response to question {idx+1}']
+                    conn.execute("INSERT INTO survey (question, response) VALUES (?, ?)", (question, response))
+
+            # Display a success message
+            st.success('Survey submitted successfully!')
 
 if __name__ == '__main__':
     main()
